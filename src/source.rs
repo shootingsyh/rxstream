c034584d18fcaf4a373d4735a184cf4ea1965ff1
Yonghong Shi <yongshi@fb.com>
2019-04-23 22:28:52 -0700

    add two test cases

--- src/source.rs
+++ src/source.rs
@@ -1,11 +1,18 @@
 use futures::Stream;
 use tokio::timer::Interval;
+use std::time::{Duration, Instant};
+
+pub fn timer(initial: u64, period: u64) -> impl Stream<Item = u64, Error = tokio::timer::Error> {
+    let iter = 0u64..;
+    Interval::new(
+        Instant::now() + Duration::from_millis(initial), 
+        Duration::from_millis(period),
+    ).zip(futures::stream::iter_ok(iter)).map(|r| r.1)
+}
+
 
 pub fn interval(millis: u64) -> impl Stream<Item = u64, Error = tokio::timer::Error> {
-    let iter = 0u64..;
-    Interval::new_interval(std::time::Duration::from_millis(millis))
-        .zip(futures::stream::iter_ok(iter))
-        .map(|r| r.1)
+    timer(0, millis)
 }
 
 pub fn of<T: Iterator>(iter: T) -> impl Stream<Item = T::Item, Error = ()> {
---
+++ tests/test_sources.rs
@@ -0,0 +1,34 @@
+use rxstream::source;
+use tokio::prelude::*;
+
+use tokio::runtime::current_thread::Runtime;
+
+
+#[test]
+fn of_generates_list() {
+    let f = source::of(0..).take(5).collect();
+    let r = f.wait();
+    assert_eq!(r, Ok(vec![0,1,2,3,4]))
+}
+#[test]
+fn timer_generates_list() {
+    // need tokio run time for timer
+    let mut runtime = Runtime::new().unwrap();
+    let t = source::interval(10)
+        .take(3)
+        .collect();
+    let r = runtime.block_on(t).unwrap();
+    assert_eq!(r, vec![0,1,2])
+}
+
+#[test]
+fn timer_generates_timed_list() {
+    //generating two timed list, and see their order
+    //need tokio run time for timer
+    let mut runtime = Runtime::new().unwrap();
+    let t1 = source::interval(10).take(3);
+    let t2 = source::timer(3, 10).take(6);
+    let t = t1.select(t2).collect();
+    let r = runtime.block_on(t).unwrap();
+    assert_eq!(r, vec![0, 0, 1, 1, 2, 2, 3, 4, 5])
+}
